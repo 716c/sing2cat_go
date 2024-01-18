75b48b19b173f5adc945848e7d1cd0c86602b2e6
ProjechAnonym <151177899+ProjechAnonym@users.noreply.github.com>
2024-01-18 22:04:57 +0800

    Update Load.go

--- goproxy/initial/Load.go
+++ goproxy/initial/Load.go
@@ -7,14 +7,14 @@ import (
 	"github.com/bitly/go-simplejson"
 )
 func loadTemplate(){
-	file,err := os.ReadFile(filepath.Join(GetValue("base_dir").(string),"\\config\\static\\template.json"))
+	file,err := os.ReadFile(filepath.Join(GetValue("base_dir").(string),"/config/static/template.json"))
 	ErrorLog(err,true,"读取模板文件失败")
 	file_content,err := simplejson.NewJson(file)
 	ErrorLog(err,true,"json序列化模板文件失败")
 	SetValue("template",file_content)
 }
 func loadConfig(){
-	file,err := os.ReadFile(filepath.Join(GetValue("base_dir").(string),"\\config\\config.json"))
+	file,err := os.ReadFile(filepath.Join(GetValue("base_dir").(string),"/config/config.json"))
 	ErrorLog(err,true,"读取配置文件失败")
 	file_content,err := simplejson.NewJson(file)
 	ErrorLog(err,true,"json序列化配置文件失败")
